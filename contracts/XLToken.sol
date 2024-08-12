77db81cafd0cf84ae4a0cf5524eb6a2b603333c5
mindcarver <626984947@qq.com>
2024-08-12 09:37:32 +0800

    feat:add XL token

---
+++ contracts/XLToken.sol
@@ -0,0 +1,5 @@
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+contract XLToken is ERC20PermitUpgradeable,AccessControlUpgradeable{
+}
