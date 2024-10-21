acb9463dae7a278a40a40b3ea3039262f15cf53b
mindcarver <626984947@qq.com>
2024-10-21 10:01:09 +0800

    test

--- tasks/release.ts
+++ tasks/release.ts
@@ -9,8 +9,8 @@ task("realease", "realease xl token")
         const [deployer] = await ethers.getSigners();
 
         const MyContract = await ethers.getContractFactory("XLToken");
-        const contract = MyContract.attach("0x8E37190Bf2d959ffc4Fe0987f5890BBc7Cb3Bb2f") as XLToken;  // testnet xl
-        //const contract = MyContract.attach("0x5D6C04B0AA084c95b5F4f09fF3e9F9c5120Ef8FD") as XLToken;  // mainnet xl 
+        //const contract = MyContract.attach("0x8E37190Bf2d959ffc4Fe0987f5890BBc7Cb3Bb2f") as XLToken;  // testnet xl
+        const contract = MyContract.attach("0x5D6C04B0AA084c95b5F4f09fF3e9F9c5120Ef8FD") as XLToken;  // mainnet xl 
 
         // call releaseLinear method
         await contract.connect(deployer).releaseLinear();
