bfdce8f7bcfc4b6d11bd9d218dd2b1e9ca07d2ff
mindcarver <626984947@qq.com>
2025-01-07 11:24:37 +0800

    feat:change punk

--- contracts/Punk.sol
+++ contracts/Punk.sol
@@ -3,35 +3,55 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract Punk is ERC721URIStorage, Ownable {
-    uint256 private _tokenIdCounter;
+contract Punk is ERC721, Ownable {
+    uint256 public constant MAX_SUPPLY = 10_000_000; 
+    uint256 private _currentTokenId = 1; 
+    string private _baseTokenURI;
 
-    // 事件
     event NFTMinted(address indexed to, uint256 tokenId);
 
-    constructor() ERC721("ErbieNFT", "ENFT") Ownable(msg.sender) {}
- 
-    function mintNFT(
-        address recipient,
-        string memory tokenURI
-    ) public onlyOwner returns (uint256) {
-        _tokenIdCounter++;
-        uint256 newItemId = _tokenIdCounter;
-
-        _mint(recipient, newItemId);
-        _setTokenURI(newItemId, tokenURI); // 使用 ERC721URIStorage 提供的方法
-
-        emit NFTMinted(recipient, newItemId);
-        return newItemId;
+    constructor(
+        string memory baseURI_,
+        string memory name_,
+        string memory symbol_
+    ) ERC721(name_,symbol_) Ownable(msg.sender) {
+        _baseTokenURI = baseURI_;
     }
 
-    function getTokenURI(uint256 tokenId) public view returns (string memory) {
-        return tokenURI(tokenId); // 使用 ERC721URIStorage 提供的方法
+    function _baseURI() internal view override returns (string memory) {
+        return _baseTokenURI;
     }
 
-    function contractURI() public view returns (string memory) {
-        return "ipfs://QmTNgv3jx2HHfBjQX9RnKtxj2xv2xQDtbVXoRi5rJ3a46e";
+    function setBaseURI(string memory baseURI) public onlyOwner {
+        _baseTokenURI = baseURI;
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view virtual override returns (string memory) {
+        _requireOwned(tokenId);
+
+        string memory baseURI = _baseURI();
+        return
+            bytes(baseURI).length > 0
+                ? string(
+                    abi.encodePacked(baseURI, "/", Strings.toString(tokenId))
+                )
+                : "";
+    }
+
+    function mint(address recipient) public onlyOwner {
+        require(_currentTokenId <= MAX_SUPPLY, "Max supply reached");
+
+        _mint(recipient, _currentTokenId);
+
+        emit NFTMinted(recipient, _currentTokenId);
+        
+        _currentTokenId++;
+    }
+
+    function currentTokenId() public view returns (uint256) {
+        return _currentTokenId;
     }
 }
